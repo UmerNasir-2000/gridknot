954cd56abe5266172f7c956f4dde5724d0f2b2cc
Umer Nasir <umer.nasir@conradlabs.com>
2025-10-25 19:45:17 +0500

    Hook Database Session into Rate Limiter Middleware

--- src/middlewares/rate_limit_middleware.py
+++ src/middlewares/rate_limit_middleware.py
@@ -1,8 +1,21 @@
 from typing import Callable, Awaitable
 
-from fastapi import Request, Response
+from fastapi import Request, Response, Depends
+from sqlalchemy.orm import Session
+
+from src.database.connection import SessionLocal
+from src.entities.api_key import ApiKey
+
 
 async def rate_limiting_middleware(
         request: Request,
         call_next: Callable[[Request], Awaitable[Response]]) -> Response:
-    return await call_next(request)
+    db: Session = SessionLocal()
+    try:
+        rows = db.query(ApiKey).all()
+        print(rows)
+        response = await call_next(request)
+    finally:
+        db.close()
+
+    return response
